2aa004918f311f40311411396b437cd1ed00506a
SouravKuchlyan <skuchlyan22@gmail.com>
2025-12-06 21:31:18 +0530

    Update 2nd_Assignment.py

--- 2nd_Assignment.py
+++ 2nd_Assignment.py
@@ -4,7 +4,15 @@ num = int(input("Enter a Number :"))
 print(num, "is an Even number") if num %2 == 0 else print (num, "is an Odd number")
 
 
-# Task - 2 :-   Sum of Integers from 1 to 50 Using a Loop.
+# Task - 2 :-   Sum of Integers from 1 to 50 Using a Loop without using inbuilt sum.
+
+total = 0
+
+for i in range (1,51):
+    total += i
+print ("The Sum of the numbers from 1 to 50 is :- ", total)
+
+# Sum of Integers from 1 to 50 Using a Loop with using inbuilt sum.
 
 total = sum([i for i in range(1, 51)])
 print("The Sum of the numbers from 1 to 50 is :- ", total)
